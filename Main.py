cfce488b1259bba638dd60937750a13db53acadc
plvines <none>
2022-07-18 21:47:34 -0400

    Added generate_post_cards

--- Main.py
+++ Main.py
@@ -4,7 +4,8 @@ import drawSvg as draw
 import random
 
 from BuildRoutes import make_all_triple_routes, value_all_triples
-from Drawing import draw_route_card
+from Drawing import draw_route_card, draw_post_cards
+from Board import BOARD, POSTS_TO_RESOURCES
 
 
 def generate_contract_cards(num_cards=None):
@@ -12,3 +13,7 @@ def generate_contract_cards(num_cards=None):
     valued = value_all_triples(triple_list)
     for idx, triple in enumerate(valued):
         draw_route_card(f"cards/contracts/{idx}", triple)
+
+
+def generate_post_cards():
+    draw_post_cards(BOARD, POSTS_TO_RESOURCES)
